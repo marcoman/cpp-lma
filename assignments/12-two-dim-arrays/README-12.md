0acb37ef7c131072786e39249dcd0c05336d057c
Marco Morales. <mr.marco.a.morales@gmail.com>
2024-09-29 19:56:16 -0400

    I'm working over some array stuff, and allocating memory from the heap for my 2-d array.

--- assignments/12-two-dim-arrays/README-12.md
+++ assignments/12-two-dim-arrays/README-12.md
@@ -1,1 +1,18 @@
 # Introduction
+
+
+## Two-Dimensional Arrays 
+- Write a program which
+- Initializes a two-dimensional array
+
+See the example I have in the [test](test.cpp) application, which follows what we did in the lecture.
+
+- Prints out the value of an element from the array
+Yup.  I also took the liberty of adding a few `#define` statements to make the #rows and #columns constants.
+
+- Iterates over all the elements and prints out their elements in order
+I print the contents to the screen.
+
+## Flattened two-dimensional array
+- Repeat the previous exercise, but with the data "flattened" into a one-dimensional array
+- Why is this useful?
--- assignments/12-two-dim-arrays/test.cpp
+++ assignments/12-two-dim-arrays/test.cpp
@@ -14,8 +14,9 @@ int main(void)
         {"Smith", "Jones", "Brown", "Davis"}
     };
 
+
     cout << "names[1][2] is " << names[1][2] << endl;
-    cout << "\n The names are:";
+    cout << "\nThe names are:";
 
     for (int r = 0; r < NUM_ROWS; r++)
     {
@@ -25,5 +26,39 @@ int main(void)
         }
     }
     cout << "\n";
+
+    // This is the better example, because it uses memory allocated from the heap.
+    string** pName = new string*[NUM_ROWS];
+    for (int r = 0; r < NUM_ROWS; r++)
+    {
+        pName[r] = new string[NUM_COLS];
+    }
+
+    pName[0][0] = "nJohn";
+    pName[0][1] = "nMary";
+    pName[0][2] = "nPeter";
+    pName[0][3] = "nSue";
+    pName[1][0] = "nSmith";
+    pName[1][1] = "nJones";
+    pName[1][2] = "nBrown";
+    pName[1][3] = "nDavis";
+
+    cout << "new names[1][2] is ... " << pName[1][2] << endl;
+    cout << "new names are:";
+
+    for (int r = 0; r < NUM_ROWS; r++)
+    {
+        for (int c = 0; c < NUM_COLS; c++)
+        {
+            cout << pName[r][c] << " ";
+        }
+    }
+    cout << "\n";
+
+    // Is this right?
+    for (int r = 0; r < NUM_ROWS; r++)
+    {
+        delete[] pName[r];
+    }
     return 0;
 }
